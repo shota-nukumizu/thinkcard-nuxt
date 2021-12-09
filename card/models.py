88b5f5e60e15bcdcff9b9d58baea8427154fe8a7
shota-nukumizu <author2000.1225@gmail.com>
2021-12-09 10:30:09 +0900

    モデル作成

--- card/models.py
+++ card/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from markdownx.models import MarkdownxField
 
-# Create your models here.
+class TagModel(models.Model):
+    name = models.CharField('タグ名', max_length=255)
+    color = models.CharField('背景色', max_length=7, default='#000000')
+
+    def __str__(self):
+        return self.name
+
+class ArticleModel(models.Model):
+    username = models.CharField('投稿者', max_length=100, unique=True)
+    title = models.CharField('タイトル', max_length=255)
+    tag = models.ManyToManyField(TagModel, verbose_name='タグ')
+    slug = models.SlugField('スラッグ', unique=True)
+    main_text = MarkdownxField('本文')
+    created_at = models.DateField('作成日', auto_now=True)
+
+    class Color(models.TextChoices):
+        WHITE = '#ffffff', '白'
+        BLACK = '#000000', '黒'
+    
+    color = models.CharField(
+        '文字色',
+        max_length=7,
+        choices=Color.choices,
+        default='#ffffff'
+    )
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
